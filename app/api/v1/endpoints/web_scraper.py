8038da04bdfd36c6528fee970575b323a73875f8
Ryan Bui <57875378+ryantbvt@users.noreply.github.com>
2025-01-08 10:43:48 -0800

    Complete web scraper (#3)
    * some updates to web-scraper
    
    * Initial headline web scrape
    
    * created list of headlines. Asynchronous httpx calls, replacing requests
    
    * returning list of headlines
    
    * remove unnecessary imports
    
    * added TODO
    
    * added timeout
    
    * formatted code to look nicer
    
    * make sure that the ticker is uppercase
    
    * validate headline list length
    
    * Headline agent valid ticker (#5)
    
    * Check for valid ticker
    
    * check ticker
    
    * Code refactor and yfinance import
    
    * Request code
    
    * added comments and added yfinance to poetry
    
    ---------
    
    Co-authored-by: tesposo <esposot@gmail.com>
    Co-authored-by: tesposo <70607841+tesposo@users.noreply.github.com>

--- app/api/v1/endpoints/web_scraper.py
+++ app/api/v1/endpoints/web_scraper.py
@@ -1,10 +1,13 @@
 ''' Web scraper code '''
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from python_utils.logging.logging import init_logger
 from bs4 import BeautifulSoup
+import yfinance as yf
 
-from app.schemas.web_scraper import WebScraperConfig
+import httpx
+
+from app.schemas.web_scraper import WebScraperConfig, WebScraperResponse
 from app import paths
 
 # Initialize logger
@@ -14,19 +17,81 @@ logger = init_logger()
 router = APIRouter()
 
 # Initialize the configs
-config = WebScraperConfig.from_yaml(paths.WEB_SCRAPER_CONFIG_DIR)
+web_scraper_config = WebScraperConfig.from_yaml(paths.WEB_SCRAPER_CONFIG_DIR)
 
 ''' API '''
-@router.post('/web-scrape')
-async def scrape(ticker: str):
+@router.get('/scrape')
+async def scrape(ticker: str) -> WebScraperResponse:
     '''
     Description: Scrapes yahoo finance for news articles
+
+    Step 1: Search website and scrape using the ticker
+    Step 1.1: Validate that it's a valid ticker by checking it found articles
+    Step 2: Parse and extract article headlines
 
     Args:
         ticker (str): ticker of stock that we're scraping for
     
     Return:
-        TBD
+        headline_list_response (WebScraperResponse): list of headlines
     '''
-    
 
+    try:
+        async with httpx.AsyncClient(timeout=web_scraper_config.timeout) as client:
+            
+            # check if ticker exists
+            logger.info(f"Validing if {ticker} is valid.")
+            valid_ticker_data = yf.Ticker(ticker.upper())
+            info = valid_ticker_data.info
+            if info['trailingPegRatio'] is None:
+                raise HTTPException(status_code=400, detail='Invalid ticker')
+            
+            logger.info(f"{ticker} is valid.")
+            
+            ''' Step 1: Fetch page '''
+            # format URI
+            ticker = ticker.upper()
+            yahoo_finance_url = web_scraper_config.base_url.format(ticker=ticker)
+
+            # Request page
+            logger.info(f'Calling {yahoo_finance_url}')
+
+            yahoo_articles_raw_data = await client.get(
+                url=yahoo_finance_url, 
+                headers=web_scraper_config.headers, 
+                timeout=web_scraper_config.timeout
+            )
+            yahoo_articles_raw_data.raise_for_status()
+
+            logger.info(f'Successfully retrieved articles from: {yahoo_finance_url}')
+
+            ''' Step 2: Fetch headlines '''
+            logger.info(f"Parsing raw data")
+
+            soup = BeautifulSoup(yahoo_articles_raw_data.text, 'html.parser')
+            headlines = soup.findAll('h3', class_='clamp yf-18q3fnf', limit=None)
+
+            # Extract each string
+            logger.info("Extracting headlines from soup")
+            headline_list = [headline.text for headline in headlines]
+
+            # validate if we have the minimum
+            if len(headline_list) < web_scraper_config.min_articles:
+                raise HTTPException(
+                    f"Under minimum number of articles to run analysis. Minimum articles set to {web_scraper_config.min_articles}"
+                )
+
+            # Make sure to cap it to the max_articles
+            if len(headline_list) > web_scraper_config.max_articles:
+                headline_list = headline_list[:web_scraper_config.max_articles]
+
+            # Returning list of headlines
+            headline_list_response = WebScraperResponse(headline_list=headline_list)
+            return headline_list_response
+
+    except httpx.HTTPError as e:
+        raise HTTPException(
+            status_code=503,
+            detail=f"Failed to fetch news: {str(e)}"
+        )
+    
--- app/schemas/web_scraper.py
+++ app/schemas/web_scraper.py
@@ -2,10 +2,14 @@
 
 import yaml
 from pydantic import BaseModel
+from typing import Dict, List
 
 class WebScraperConfig(BaseModel):
     base_url: str
+    headers: Dict[str, str]
     timeout: int
+    min_articles: int
+    max_articles: int
 
     @classmethod
     def from_yaml(cls, file: str) -> 'WebScraperConfig':
@@ -13,3 +17,5 @@ class WebScraperConfig(BaseModel):
             config_dict = yaml.safe_load(f)
         return cls.model_validate(config_dict)
     
+class WebScraperResponse(BaseModel):
+    headline_list: List[str]
