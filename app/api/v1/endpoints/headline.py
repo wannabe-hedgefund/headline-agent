a63011e65f76a1b0098f7f784e64e9f851fd6b5f
Ryan Bui <ryan.bui@gmail.com>
2025-01-13 10:59:52 -0800

    Completed most of the code. Pending final fix for model gateway

--- app/api/v1/endpoints/headline.py
+++ app/api/v1/endpoints/headline.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, HTTPException
 from python_utils.logging.logging import init_logger
 import asyncio
 import yfinance as yf
+import httpx
 
 from app.schemas.headline import HeadlineRequest, HeadlineConfig
 from app.helpers.async_process import async_handler
@@ -29,8 +30,9 @@ async def predict(headline_request: HeadlineRequest):
     Step 2.1: Web-scrape news articles using ticker
     Step 2.2: Fetch historical stock price data via yfinance
     Step 3: Run sentiment analysis on news article's headlines (maybe entire article)
-    Step 4: Gather sentiment over all recent news articles and historcal price and build a prompt
-    Step 5: Send to LLM for price prediction.
+    Step 4: Process sentiment analysis and historical data
+    Step 5: Build a prompt
+    Step 6: Send to LLM for price prediction.
 
     Args:
         headline_request: user input of ticker they want to check
@@ -62,9 +64,82 @@ async def predict(headline_request: HeadlineRequest):
     ''' Step 3: Run sentiment analysis '''
     headlines_sentiments = await async_handler(
         headlines=headlines,
-        model_gateway=headline_config.model_gateway
+        model_gateway=headline_config.slm_gateway
     )
 
-    return headlines_sentiments
+    ''' Step 4: Process data '''
+    
+    # Get necessary ticker information
+    logger.info(f"Processing historical price data of {ticker}")
+    min_close = ticker_hist['Close'].min()
+    min_close_date = ticker_hist['Close'].idxmin().strftime('%Y-%m-%d')
+    max_close = ticker_hist['Close'].max()
+    max_close_date = ticker_hist['Close'].idxmax().strftime('%Y-%m-%d')
 
-    return {"message": "test"}
+    current_close = ticker_hist['Close'].iloc[-1]
+    avg_close = ticker_hist['Close'].mean()
+
+    # Reformat sentiment analysis for prompt
+    logger.info(f'Re-formatting sentiment analysis for {ticker}')
+    sentiment_summary = "Recent news analysis:\n"
+
+    # Parse through list of sentiments and format it
+    for i, sentiment in enumerate(headlines_sentiments, 1):
+        '''
+        Calculate dominant sentiment. 
+        Aka the highest scoring sentiment for a headline.
+        '''
+        sentiments = {
+            "Positive": sentiment.positive,
+            "Neutral": sentiment.neutral,
+            "Negative": sentiment.negative
+        }
+        '''
+        Lambda notes:
+        x: the tuple
+        x[0]: Positive, Neutral, Negative
+        x[1]: sentiment score
+
+        So below method is comparing the sentiment scores to each other, finding the max value.
+        We are returning the tuple, {Postive, 0.7} for example, as the max value.
+        '''
+        dominant_sentiment = max(sentiments.items(), key=lambda x: x[1])
+
+        sentiment_summary += f"""\
+            Article: {i}:
+            Headline: {sentiment.headline}
+            Dominate Sentiment: {dominant_sentiment[0]} ({dominant_sentiment[1]:.2f})
+            Sentiment Distribution: {sentiment.positive:.2f} | Neu: {sentiment.neutral:.2f} | Neg: {sentiment.negative:.2f}
+            """
+
+    logger.info(f"Completed processing data for {ticker}. Building prompt.")
+
+    ''' Step 5: Build prompt '''
+    formatted_prompt = headline_config.llm_config.prompt_template.format(
+        ticker=ticker,
+        max_close=max_close,
+        max_close_date=max_close_date,
+        min_close=min_close,
+        min_close_date=min_close_date,
+        current_close=current_close,
+        avg_close=avg_close,
+        sentiment_summary=sentiment_summary
+    )
+
+    ''' Step 6: Send prompt to LLM '''
+    logger.info(f"Sending request to LLM")
+
+    llm_payload = {
+        "model_name": headline_config.llm_config.llm_model,
+        "prompt": formatted_prompt,
+        "temperature": headline_config.llm_config.temperature
+    }
+
+    async with httpx.AsyncClient() as client:
+        llm_response = await client.post(
+            url=headline_config.llm_gateway,
+            json=llm_payload
+        )
+
+
+    return llm_response
--- app/schemas/headline.py
+++ app/schemas/headline.py
@@ -6,19 +6,24 @@ import yaml
 class HeadlineRequest(BaseModel):
     stock_ticker: str
 
+class LLMConfig(BaseModel):
+    llm_model: str
+    temperature: float
+    prompt_template: str
+
 class HeadlineConfig(BaseModel):
     web_scraper_dns: str
-    model_gateway: str
+    slm_gateway: str
+    llm_gateway: str
     sentiment_model: str
-    llm_model: str
-    prompt_template: str
+    llm_config: LLMConfig
 
     @classmethod
     def from_yaml(cls, file: str) -> 'HeadlineConfig':
         with open(file, "r") as f:
             config_dict = yaml.safe_load(f)
         return cls.model_validate(config_dict)
-    
+
 class RobertaSentimentResponse(BaseModel):
     headline: str
     negative: float
